bbcf6ce12f1ed9b9130867d0ae0ae766b451ed9e
Matthew Hardenburgh <matthew@hardenburgh.io>
2025-02-14 23:31:22 -0800

    created testing framework

---
+++ testing/load/loadTest.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <gtest/gtest.h>
+#include "testFramework.h"
+
+TEST(loadTest, load_byte_unsigned)
+{
+    testFramework framework("load_byte_unsigned");
+
+    // true if output matches expected
+    EXPECT_TRUE(framework.run());
+}
