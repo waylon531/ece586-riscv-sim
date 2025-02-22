af18bf323308e27fd6a613865b8ef85610d57d09
Waylon Cude <waylon.cude@finzdani.net>
2025-02-21 21:13:30 -0800

    Add basic makefile
    This captures all the toolchain jank so far, might be good to expand on
    it in the future though

--- programs/make_mem.sh
+++ programs/make_mem.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 
-file="${1%.*}"
+file="$1"
+stripped="${1%.*}"
 if ! [[ -e $file ]] ; then
     echo "File $file not found"
     exit 1
 fi
 # I dunno if these are all possible sections
-riscv32-unknown-elf-objdump -j .text -j .data -j .bss -j .got -D "$file" | grep -o '^[[:blank:]]*[[:xdigit:]]*:[[:blank:]][[:xdigit:]]*' > "${file}.mem"
+riscv32-unknown-elf-objdump -j .text -j .data -j .bss -j .got -D "$file" | grep -o '^[[:blank:]]*[[:xdigit:]]*:[[:blank:]][[:xdigit:]]*' > "${stripped}.mem"
 
 echo "Done!"
