060353daf14431f6dcc47e59855ed5e46e8e9ee3
Matthew Hardenburgh <matthew@hardenburgh.io>
2025-03-15 00:35:01 -0700

    update framework to use different RISCV compilers

--- testing/testFramework/testFramework.h
+++ testing/testFramework/testFramework.h
@@ -3,8 +3,21 @@
 class testFramework
 {
     public:
+        /**
+         * @brief default constructor
+        */
         testFramework();
+        /**
+         * @brief construct test framework with paths, testname and test folder
+         * @param simBinaryLocation is the location of the simulation
+         * @param rootPath is the location of the location of the root of the repository
+         * @param testName is the name of the test
+         * @param is the instructionType, the overal category folder it resides under
+        */
         testFramework(std::string simBinaryLocation, std::string rootPath, std::string testName, std::string instrType);
+        /**
+         * @brief deconstructor, cleans up the files created during the test
+        */
         ~testFramework();
 
         bool run();
@@ -14,7 +27,14 @@ class testFramework
          * @brief compare line by line through the simResult and expectedResult
         */
         void parseResult();
+        /**
+         * @brief parses the .dis disassembly file and generates a mem image file
+        */
         void generateMemImage();
+        /**
+         * @brief finds the paths of the of the specified file in your $PATH variable
+         * @param fileName is a regex pattern for the file you want to find in your $PATH
+        */
         std::string getPath(std::string fileName);
         std::string m_testName;
         bool pass = false;
