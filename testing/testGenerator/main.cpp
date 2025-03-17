53abc5cfd29f4f0e3ff5d37d0bbb4f35b627263d
Waylon Cude <waylon.cude@finzdani.net>
2025-03-17 15:15:12 -0700

    Do some fixes to make the test framework work
    On my machine parsing the path fails. There's no need to do it here so
    just don't call getPath. The other fix makes it so the testgenerator can
    work by default without extra steps.
    
    I symlinked ~/ece586-riscv-sim to where my git repo is so making that
    part relative is a low priority for me now

--- testing/testGenerator/main.cpp
+++ testing/testGenerator/main.cpp
@@ -7,7 +7,7 @@ int main()
     testGenerator integer("../integer/testResources/assembly", "../integer/integerTest.cpp", "integer");
     testGenerator branchJump("../branchJump/testResources/assembly", "../branchJump/branchJumpTest.cpp", "branchJump");
 
-    system("cd ../; cmake --build build");
+    system("cd ../; cmake -S . -B build; cmake --build build");
 
     return 0;
 }
