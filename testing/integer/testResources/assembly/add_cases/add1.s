3219a0775ed853a20b3e0063b3d91d25cdaa4858
Julay Leatherman-Brooks <julay@pdx.edu>
2025-03-16 18:28:33 -0700

    removing deleted files/old filenames

--- testing/integer/testResources/assembly/add_cases/add1.s
+++
@@ -1,4 +0,0 @@
-addi t0, zero, 10
-addi t1, zero, 6
-add t2, t1, t0 ; case analysis 1: operands > 0, same sign bit. 
-               ; expected register state: t0 <-- 0x0000_000A, t1 <-- 0x0000_0006, t2 <-- 0x0000_0010
