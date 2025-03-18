f1eea5806977878610b31515003a7057f93ca9d3
julayleathermanbrooks <julaybrooks@me.com>
2025-03-18 12:33:34 -0700

    added all branch tests except bge and bgeu

---
+++ testing/branchJump/testResources/assembly/blt_false_eq.s
@@ -0,0 +1,5 @@
+addi t0, zero, 1
+addi t1, zero, 1
+addi t2, zero, 32 
+addi s0, t2, 16 
+blt t0, t1, s0
---
+++ testing/branchJump/testResources/assembly/bltu_true_diff_msb.s
@@ -0,0 +1,5 @@
+addi t0, zero, 1 
+addi t1, zero, 268435456 
+addi t2, zero, 32 
+addi s0, t2, 16 
+bltu t0, t1, s0
