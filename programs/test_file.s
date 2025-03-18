ecdd5f5f3ac347cebfab612ff47fb7956c3d35c3
kay <kayhyn@pdx.edu>
2025-03-18 03:23:27 -0700

    fixed file handling

--- programs/test_file.s
+++ programs/test_file.s
@@ -1,13 +1,25 @@
 .global main
 .data
+filename:
+  .string "/tmp/test.out"
 msg:
-  .string "Hello World\n"
-  .byte 0
+  .string "Hello World"
+output:
+  .space 12
 .text
 main:
-li a0, 1
+li a7, 56
+la a0, filename
+li a1, 0x40
+ecall
+li a7, 64
 la a1, msg
-la a2, 12
+li a2, 12
+ecall
+li a7, 63
+la a1, output
+ecall
+li a0, 1
 li a7, 64
 ecall
 jr ra
